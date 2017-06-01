bff9b6a8b531458a5fb729504de86f695fd9f24f
Wesley <wmcmilla@lsoc.org>
2017-06-01 17:26:17 -0500

    Initial progarm upload. Thank you for you time

---
+++ Hackathon.py
@@ -0,0 +1,66 @@
+#start of game
+print("Welcome to Hackathon")
+# Goal is to hack various groups u
+print("Your goal is to hack goverments, terroists, secuirty teams and comapines.")
+print("DISCALIMER. THIS GAME IS NOT REALLY HACKING> DO NOT HACK ANY OF THE GROUPS YOU CAN HACK IN THE GAME.")
+print("Pre-Alpha")
+print("All riddles are from riddles.com")
+dollars = 25000
+hackgroup = input("Type in the name of the group you want to hack. (eg. ZIA (Tutorial) , Z16(Story , Jackson Palm Casino (Story), Northern United Bank (Story), Fort Michealson (Story):")
+if hackgroup == "ZIA":
+    print("Entering ZIA mainframe....Load complete. Password requierd for group 90-89029 access. Enter string.")
+    passCIA1 = input("Password:")
+    if passCIA1 == "penny":
+        print("Correct Loading...")
+    else:
+        print("You have failed. The password is the answer to this riddle. What has a head, a tail is brown, and has no legs?")
+        answertorid1 = input("Answer to riddle:")
+        if answertorid1 == "penny":
+            print("ZIa secure files tarnsefring. You need another code to veiw files.")
+            passCIA2 = input("Enter code:")
+            if passCIA2 == "The living room":
+                print("n")
+            else:
+                print("You got it wrong. The code is the answer to a riddle what room do ghosts avoid:")
+                passCIA3 = input("Answer:")
+                if passCIA3 == "the living room":
+                     print("Printing files")
+                     print("List of files.5590 condifental files tarnsferd. To shut down mainframe secuirty sysytem enter code:")
+                     shutdownss = input("Answer.")
+                     if shutdownss == "ice":
+                         print("n")
+                     else:
+                         print("Hint: What is not dry nor wet but can be both.")
+                         passCIA4 = input("Answer:")
+                         if passCIA4 == "ice":
+                             print("N")
+                         
+                         else:
+                             print("Wrong have another riddle:")
+                             print("What has many keys, but can't even open a single door?")
+                             passCIA5 = input("Answer:")
+                             if passCIA5 == "piano":
+                                 print("n")
+                             else:
+                                 print("N")
+                                 
+                             
+                         
+if hackgroup == "Jackson Palm Casino":
+    print("Incoming Message from Chad.")
+    declineorapmission1 =  input("Decline Call or Accept Call?")
+    if declineorapmission1 == "Accept":
+        print("I hear you are hoping to hack the Jackson Palm Casino. Wait, I forgot to introduce myself. I am Chad, former ZIB agent. Now I plan robberies for my friend Highlow. He needs a hacker, so call me back if you want to do the job.")
+        print("You have started story mode, now you will have many items such as money and the ability to join and create jobs. You will start with 25,000 dollars.")
+        callchad1 = input("Call Chad back to accept job?")
+        if callchad1 == "Call back":
+            print("Hey, I want to work the job")
+            
+    
+        
+
+
+
+            
+        
+        
